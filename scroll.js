7e7330019d89cef3a7a0cc78ea22ea47b7e47e90
c-a-e-z-a-r <jamescaezar123@gmail.com>
2025-02-13 00:25:09 +0800

    b

--- scroll.js
+++ scroll.js
@@ -2,12 +2,17 @@ window.addEventListener("scroll", function () {
     const title = document.querySelector("h1");
     const content = document.querySelector(".content");
     const button = document.querySelector("button");
-    
+    const container = document.querySelector(".container");
+
     if (window.scrollY > 50) {
         title.classList.add("shrink");
         content.classList.add("fade-in");
         button.classList.add("fade-in");
+        container.classList.add("shrink-container");
     } else {
         title.classList.remove("shrink");
+        content.classList.remove("fade-in");
+        button.classList.remove("fade-in");
+        container.classList.remove("shrink-container");
     }
 });
